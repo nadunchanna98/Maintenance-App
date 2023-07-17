0c96ffb0104f6310ec7f6d794c4e6b8a475c783b
AnukaMithara <anukamithara@gmail.com>
2023-07-17 21:11:40 +0530

    modified labor assign page

--- FrontEnd/src/Common/BaseURL.js
+++ FrontEnd/src/Common/BaseURL.js
@@ -4,7 +4,7 @@ let BASE_URL = "";
 
 // // sachira
 if (Platform.OS === "android") {
-     BASE_URL = "http://10.102.13.22:3000/api/v1/" // for android emulator
+     BASE_URL = "http://192.168.157.8:3000/api/v1/" // for android emulator
 } else {
      BASE_URL = "http://192.168.171.74:3000/api/v1/" // for ios emulator
 }
