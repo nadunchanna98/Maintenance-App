2d6538e7d52d068ad11555d38b9dfb613774c98d
nadunchanna98 <gancp1218@gmail.com>
2023-07-22 15:54:53 +0530

    image uploading done

--- Backend/models/Complains.js
+++ Backend/models/Complains.js
@@ -1,62 +1,70 @@
 const mongoose = require('mongoose');
 
 const ComplaintSchema = new mongoose.Schema({
-    userID: { 
+    userID: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User_Details'
-     },
-     title: { 
-        type: String, 
-        required: false
-     },
-     location: {
+    },
+    title: {
         type: String,
         required: false
-        },
-     supervisorID: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Supervisor_Details' 
+    },
+    location: {
+        type: String,
+        required: false
+    },
+    subLocation: {
+        type: String,
+        required: false
+    },
+    complaineImages: {
+        type: [String],
+        default: []
+    },
+    supervisorID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Supervisor_Details'
     },
     labourerID: {
 
-        type: [mongoose.Schema.Types.ObjectId], 
+        type: [mongoose.Schema.Types.ObjectId],
         default: [],
-        ref: 'Labour_Details' 
+        ref: 'Labour_Details'
     },
-    description: { 
-        type: String, 
-        required: true 
+    description: {
+        type: String,
+        required: true
     },
     admin_description: {
         type: String
     },
-    status: { 
-        type: String, 
-        enum: ['Pending', 'AssignedA', 'AssignedS','AssignedL','CompletedL','CompletedS','CompletedA','Completed','DeclinedL','DeclinedS','DeclinedA','Declined'],
-        default: 'Pending' 
+    status: {
+        type: String,
+        enum: ['Pending', 'AssignedA', 'AssignedS', 'AssignedL', 'CompletedL', 'CompletedS', 'CompletedA', 'Completed', 'DeclinedL', 'DeclinedS', 'DeclinedA', 'Declined'],
+        default: 'Pending'
     },
-    rate : {
+    rate: {
         type: Number,
         default: 0
     },
     created_date: {
-            type: Date,
-            required: true,
-            default: Date.now
+        type: Date,
+        required: true,
+        default: Date.now
     },
-    supervisor_feedback: { 
+    supervisor_feedback: {
         type: String
     },
-    admin_feedback: { 
+    admin_feedback: {
         type: String
     },
-    assigned_date: { 
-        type: Date 
+    assigned_date: {
+        type: Date
     },
-    resolved_date: { 
-        type: Date 
+    resolved_date: {
+        type: Date
     }
-  });
+});
 
 
 exports.Complaine_Details = mongoose.model('complain-details', ComplaintSchema);
--- FrontEnd/Pages/User/ComplainPreview.js
+++ FrontEnd/Pages/User/ComplainPreview.js
@@ -1,59 +1,161 @@
 import React, { useContext, useState } from 'react';
-import { View, Alert, Text, Image, StyleSheet, TouchableOpacity, ScrollView, Dimensions } from 'react-native';
+import { View, Alert, Text, Image, StyleSheet, TouchableOpacity, ScrollView, Dimensions , ActivityIndicator } from 'react-native';
 import { AuthContext } from '../../src/Context/AuthContext';
 import { useNavigation } from '@react-navigation/native';
 import axios from 'axios';
 import BASE_URL from '../../src/Common/BaseURL';
 import moment from 'moment';
+const { width, height } = Dimensions.get('window');
+import { firebase } from '../../src/Common/config';
+
+const Slideshow = ({ images }) => {
+
+  // console.log('inside of slider', images);
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleSlide = (event) => {
+    const slideSize = event.nativeEvent.layoutMeasurement.width;
+    const currentIndex = event.nativeEvent.contentOffset.x / slideSize;
+    setCurrentIndex(Math.round(currentIndex));
+  };
+
+  return (
+    <View style={styles.slideshowContainer}>
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        onMomentumScrollEnd={handleSlide}
+      >
+        {images.map((imageUri, index) => (
+          <Image
+            key={index}
+            source={{ uri: imageUri }}
+            style={styles.slideshowImage}
+            resizeMode="cover"
+          />
+        ))}
+      </ScrollView>
+      <View style={styles.paginationContainer}>
+        {images.map((_, index) => (
+          <View
+            key={index}
+            style={[
+              styles.paginationDot,
+              index === currentIndex ? styles.activeDot : null,
+            ]}
+          />
+        ))}
+      </View>
+    </View>
+  );
+};
 
 const ComplainPreview = ({ route }) => {
+
   const navigation = useNavigation();
-  const { title, location, description, imageUri } = route.params;
+  const { title, location,subLocation, description, selectedImages } = route.params;
+
+  const imageUri = selectedImages[0];
+
   const { userInfo } = useContext(AuthContext);
   const currentDate = moment().format('MMMM DD, YYYY');
   const currentTime = moment().format('hh:mm A');
   const [showScaledImage, setShowScaledImage] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const [loading, setLoading] = useState(false);
 
-  const handleDataSubmission = () => {
+  const uploadImageToFirebase = async (uri) => {
+    try {
+      const response = await fetch(uri);
+      const blob = await response.blob();
+      const filename = uri.substring(uri.lastIndexOf('/') + 1);
+      const ref = firebase.storage().ref().child(filename);
+      const uploadTask = ref.put(blob);
+  
+      return new Promise((resolve, reject) => {
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            setProgress(progress);
+          },
+          (error) => {
+            console.log(error);
+            reject(error);
+          },
+          async () => {
+            try {
+              await uploadTask;
+              const url = await ref.getDownloadURL();
+              resolve(url);
+            } catch (error) {
+              console.log("Error getting download URL:", error);
+              reject(error);
+            }
+          }
+        );
+      });
+    } catch (error) {
+      console.log("Error uploading image:", error);
+      throw error;
+    }
+  };
+  
+  const uploadImagesToFirebase = async (imageUris) => {
+    try {
+      const uploadedUrls = await Promise.all(imageUris.map(uploadImageToFirebase));
+      return uploadedUrls;
+    } catch (error) {
+      console.log("Error uploading images:", error);
+      throw error;
+    }
+  };
+  
+  const handleDataSubmission = async () => {
     console.log('Data submitted:', {
       userID: userInfo.userId,
       title,
       location,
+      subLocation,
       description,
       status: 'Pending',
-      imageUri,
     });
-
-    // console.log('Data submitted:', userInfo.userId );  
-
-
-    // API call to submit data
-    axios.post(`${BASE_URL}complains/add`, {
-      userID: userInfo.userId,
-      title,
-      location,
-      description,
-      status: 'Pending',
-      imageUri,
-    })
-      .then((res) => {
-        Alert.alert('Complain Submitted Successfully');
-        // navigation.navigate('ViewComplain', { complainId: res.data._id });
-        navigation.navigate('UserDashboard');
-      }
-      )
-      .catch((err) => {
-        console.log(`get users error : ${err}`);
-        //  Alert.alert('Complain Submission Failed');
-      }
-      );
-
-
+  
+    try {
+      setLoading(true);
+      const firebaseLinks = await uploadImagesToFirebase(selectedImages);
+      console.log('Firebase links:', firebaseLinks);
+  
+      // API call to submit data
+      await axios.post(`${BASE_URL}complains/add`, {
+        userID: userInfo.userId,
+        title,
+        location,
+        subLocation,
+        description,
+        status: 'Pending',
+        complaineImages: firebaseLinks,
+      });
+  
+      Alert.alert('Complain Submitted Successfully');
+      navigation.navigate('UserDashboard');
+    } catch (error) {
+      console.log('Error during data submission:', error);
+      Alert.alert('Error submitting complain');
+    }
+    finally {
+      setLoading(false);
+    }
   };
+  
+
   const handleEditData = () => {
     navigation.navigate('ComplainForm', {
       title,
       location,
+      subLocation,
       description,
       imageUri,
     });
@@ -67,9 +169,25 @@ const ComplainPreview = ({ route }) => {
     <View style={styles.container}>
 
       <ScrollView contentContainerStyle={styles.contentContainer}>
-        <TouchableOpacity onPress={handleImagePress}>
-          <Image source={{ uri: imageUri }} style={styles.image} />
-        </TouchableOpacity>
+
+
+        
+        <View style={styles.imageContainer}>
+          {selectedImages.length > 0 ? (
+            <Slideshow images={selectedImages} />
+          ) : (
+            <Image
+              source={require('../../assets/icon.png')}
+              style={styles.image}
+            />
+          )}
+        </View>
+
+
+
+
+
+
         <View style={styles.dataContainer}>
           <View style={styles.fieldContainer}>
             <Text style={styles.fieldTitle}>Complainer Name:</Text>
@@ -103,6 +221,12 @@ const ComplainPreview = ({ route }) => {
             <Text style={styles.fieldTitle}>Location:</Text>
             <Text style={styles.fieldValue}>{location}</Text>
           </View>
+          <View style={styles.fieldContainer}>
+            <Text style={styles.fieldTitle}>sub Location:</Text>
+            <Text style={styles.fieldValue}>{subLocation}</Text>
+          </View>
+
+          
 
           <View style={styles.bottomLine} />
 
@@ -112,18 +236,24 @@ const ComplainPreview = ({ route }) => {
           <Text style={styles.fieldValue}>{description}</Text>
           <View style={styles.bottomLine} />
 
-        </View>        
+        </View>
       </ScrollView>
 
-      
+
       <View style={styles.buttonContainer}>
-      <TouchableOpacity style={styles.button} onPress={handleDataSubmission}>
-        <Text style={styles.buttonText}>Submit</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={handleEditData}>
-        <Text style={styles.buttonText}>Edit</Text>
-      </TouchableOpacity>
-    </View>
+        <TouchableOpacity style={styles.button} onPress={handleDataSubmission}>
+          <Text style={styles.buttonText}>Submit</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={handleEditData}>
+          <Text style={styles.buttonText}>Edit</Text>
+        </TouchableOpacity>
+
+
+        {loading && (
+          <View style={styles.loadingContainer}>
+            <ActivityIndicator size="large" color="#0000ff" />
+          </View> )}
+      </View>
 
       {imageUri && showScaledImage && (
         <TouchableOpacity style={styles.scaledImageContainer} onPress={() => setShowScaledImage(false)}>
@@ -214,6 +344,47 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+  },
+  imageContainer: {
+    marginBottom: Dimensions.get('window').height * 0.05,
+    marginTop: Dimensions.get('window').height * 0.05,
+  },
+  placeholderImage: {
+    width: Dimensions.get('window').width * 0.5,
+    height: Dimensions.get('window').width * 0.5,
+    alignSelf: 'center',
+  },
+  imageStyle: {
+    width: width * 0.5,
+    height: width * 0.5,
+    alignSelf: 'center',
+  },
+  slideshowImage: {
+    width: width * 0.9,
+    height: width * 0.5,
+
+  },
+  slideshowContainer: {
+    width: width * 0.9,
+    height: width * 0.5,
+    alignSelf: 'center',
+  },
+  paginationContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 5,
+  },
+  paginationDot: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: 'rgba(0, 0, 0, 0.4)',
+    marginHorizontal: 3,
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
